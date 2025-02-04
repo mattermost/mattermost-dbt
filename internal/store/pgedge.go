f76e4f00def5ac7d1d02df1d4ec1fba6ea6b9e70
Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>
2025-02-04 09:06:27 -0500

    Add functionality to create missing database indexes
    When using the verify-index command a flag is now provided which
    can be used to create database indexes on nodes where they are
    found to be missing.

--- internal/store/pgedge.go
+++ internal/store/pgedge.go
@@ -13,6 +13,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type PgedgeNodeStores []*PgedgeNodeStore
+
 type PgedgeNodeStore struct {
 	Node  *model.PgedgeNode
 	Store *SQLStore
@@ -37,7 +39,7 @@ func NewStoreForPgedgeNode(db *model.PgedgeDatabase, node *model.PgedgeNode, log
 	return store, nil
 }
 
-func NewStoresForAllPgedgeNodes(pgedgeConfig *model.PgedgeClusterConfig, logger log.FieldLogger) ([]*PgedgeNodeStore, error) {
+func NewStoresForAllPgedgeNodes(pgedgeConfig *model.PgedgeClusterConfig, logger log.FieldLogger) (PgedgeNodeStores, error) {
 	nodes := pgedgeConfig.NodeGroups
 	if len(nodes) == 0 {
 		return nil, errors.New("config contains no database nodes")
@@ -57,4 +59,14 @@ func NewStoresForAllPgedgeNodes(pgedgeConfig *model.PgedgeClusterConfig, logger 
 	}
 
 	return nodesStores, nil
+}
+
+func (ns *PgedgeNodeStores) GetStoreForNode(name string) (*SQLStore, error) {
+	for _, nodeStore := range *ns {
+		if nodeStore.Node.Name == name {
+			return nodeStore.Store, nil
+		}
+	}
+
+	return nil, errors.Errorf("no node store for node %s", name)
 }
